df31ec1453710574833430a09a41d3e5b6fc449b
ime029 <ime029@gmail.com>
2025-08-02 21:12:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ roll_input = st.text_input("🔎 Roll Number (e.g., ME211001):")
 
 if roll_input:
     roll_input = roll_input.strip().upper()
-    filtered = df[df["Reg. No."] == roll_input]
+    filtered = df[df["Registration No."] == roll_input]
 
     if not filtered.empty:
         st.success(f"✅ Found {len(filtered)} course(s) for {roll_input}")
